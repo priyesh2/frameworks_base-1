0b9f161819ba776407a35389ac1cc8c9fba76dd2
Matt Mower <mowerm@gmail.com>
2014-12-31 15:02:58 -0600

    Keyguard: Forward port lockscreen quick unlock (1/2)
    * PasswordTextView QuickUnlock listener functions and method thanks to
      ParanoidAndroid project:
        Keyguard: Implement quick unlock feature [1/2]
        BigBrother1984 <carlosavignano@aospa.co>
        I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
    @mydongistiny: ported to oreo/android10
    Change-Id: Iaa140723ffb037062d565127257a7cb60526f25e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3396,7 +3396,21 @@ public final class Settings {
             "lock_pattern_tactile_feedback_enabled";
 
         /**
-         * A formatted string of the next alarm that is set, or the empty string
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT =
+                "lockscreen_scramble_pin_layout";
+
+        /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL =
+                "lockscreen_quick_unlock_control";
+
+        /**
+       * A formatted string of the next alarm that is set, or the empty string
          * if there is no alarm set.
          *
          * @deprecated Use {@link android.app.AlarmManager#getNextAlarmClock()}.
