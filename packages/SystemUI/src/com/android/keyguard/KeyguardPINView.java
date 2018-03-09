eae55bf99e2c6a54713226592d4c088abbdb916a
carlosavignano <carlosavignano@aospa.co>
2018-03-09 04:08:46 +0000

    base: Keyguard: Hide check button when using PIN quick unlock
    Change-Id: I6426bd4bf79fbb5447d337489d086eaf0a3d802b

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -166,6 +166,8 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         } else {
             mPasswordEntry.setQuickUnlockListener(null);
         }
+
+        setButtonVisibility(getOkButton(), !quickUnlock);
     }
 
     @Override
